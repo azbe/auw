e586df725a9864806bc9719bd65c1dd59cb028e2
azbe <tudorpetreradu@gmai.com>
2017-06-08 09:42:37 +0300

    More

--- index.js
+++ index.js
@@ -22,25 +22,9 @@ io.on('connection', (socket) =>
 	socket.on('chat message', (msg, timestamp) =>
 	{
 		if (msg.length > 0)
-			io.emit('chat message', stringToColor(socket.id), msg, timestamp);
+			socket.broadcast.emit('chat message', socket.id, msg, timestamp);
 	});
 
 });
 
 http.listen(2242, () => {});
-
-function stringToColor (str) 
-{
-	var hash = 0;
-	for (var i = 0; i < str.length; i++) 
-	{
-		hash = str.charCodeAt(i) + ((hash << 5) - hash);
-	}
-	var color = '#';
-	for (var i = 0; i < 3; i++) 
-	{
-  		var value = (hash >> (i * 8)) & 0xFF;
-    	color += ('00' + value.toString(16)).substr(-2);
-	}
-	return color;
-}
--- public/client.js
+++ public/client.js
@@ -4,8 +4,13 @@ $(() =>
 {
 	$('#input_send').click(() =>
 	{
-		socket.emit('chat message', $('#input_text').val(), (new Date()).toUTCString());
+		var timestamp = (new Date()).toUTCString();
+		var msg = $('#input_text').val();
+		socket.emit('chat message', msg, timestamp);
 		$('#input_text').val('');
+		$('#messages').append($('<br>'));
+		$('#messages').append($('<div class="message own" style="background: ' + stringToColor(socket.id) + ';">').text(msg));				
+		$('#messages').append($('<div class="message_time own">').text(timestamp));	
 		return false;
 	});
 
@@ -15,11 +20,30 @@ $(() =>
 			$('#input_send').click();
 	});
 
-	socket.on('chat message', (color, msg, timestamp) =>
+	socket.on('chat message', (id, msg, timestamp) =>
 	{
-		$('#messages').append($('<br>'));
-		$('#messages').append($('<div class="message" style="background: ' + color + ';">').text(msg));
-		$('#messages').append($('<div class="message_time">').text(timestamp));		
+		//if (socket.id != id)
+		{
+			$('#messages').append($('<br>'));
+			$('#messages').append($('<div class="message" style="background: ' + stringToColor(id) + ';">').text(msg));
+			$('#messages').append($('<div class="message_time">').text(timestamp));		
+		}	
 	});
 
 });
+
+function stringToColor (str) 
+{
+	var hash = 0;
+	for (var i = 0; i < str.length; i++) 
+	{
+		hash = str.charCodeAt(i) + ((hash << 5) - hash);
+	}
+	var color = '#';
+	for (var i = 0; i < 3; i++) 
+	{
+  		var value = (hash >> (i * 8)) & 0xFF;
+    	color += ('00' + value.toString(16)).substr(-2);
+	}
+	return color;
+}
